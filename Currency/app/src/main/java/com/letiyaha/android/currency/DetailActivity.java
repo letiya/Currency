45aa3026ad157b1eded2d7dad78547158cd591b6
Belle Lee <letiya66@gmail.com>
2019-08-17 20:39:09 -0700

    Apply preference settings to the formula to get a suggested buying amount for the user. #2

--- Currency/app/src/main/java/com/letiyaha/android/currency/DetailActivity.java
+++ Currency/app/src/main/java/com/letiyaha/android/currency/DetailActivity.java
@@ -2,7 +2,9 @@ package com.letiyaha.android.currency;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -21,6 +23,7 @@ import com.letiyaha.android.currency.database.InvestmentEntry;
 import com.letiyaha.android.currency.utilities.AppExecutors;
 import com.letiyaha.android.currency.utilities.Util;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -49,8 +52,11 @@ public class DetailActivity extends AppCompatActivity {
 
     private static final String CLICKED_CURRENCY = "clickedCurrency";
     private static final String MAIN_CURRENCY = "mainCurrency";
+    private final double[] INVEST_RATIO = {0.06, 0.1, 0.2, 0.28, 0.36}; // Assume 5 currencies investment for now
 
     private float mRateToday;
+
+    private String mClickedCurrency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +68,7 @@ public class DetailActivity extends AppCompatActivity {
         mLineChart = (LineChart) findViewById(R.id.lc_month);
 
         Intent intentThatStartedThisActivity = getIntent();
-        final String clickedCurrency = intentThatStartedThisActivity.getStringExtra(CLICKED_CURRENCY);
+        mClickedCurrency = intentThatStartedThisActivity.getStringExtra(CLICKED_CURRENCY);
         String mainCurrency = intentThatStartedThisActivity.getStringExtra(MAIN_CURRENCY);
 
         final List<String> xAxisValues = new ArrayList<>();
@@ -71,7 +77,7 @@ public class DetailActivity extends AppCompatActivity {
         AppExecutors.getInstance().DiskIO().execute(new Runnable() {
             @Override
             public void run() {
-                List<CurrencyEntry> data = mDb.currencyDao().loadAllAfter(clickedCurrency, Util.getNDaysAgo(Util.NUM_OF_HISTORY_DATA));
+                List<CurrencyEntry> data = mDb.currencyDao().loadAllAfter(mClickedCurrency, Util.getNDaysAgo(Util.NUM_OF_HISTORY_DATA));
 
                 for (int i = 0; i < data.size(); i++) {
                     CurrencyEntry currencyEntry = data.get(i);
@@ -79,20 +85,19 @@ public class DetailActivity extends AppCompatActivity {
                     SimpleDateFormat sdf = new SimpleDateFormat("MMdd");
                     String dateString = sdf.format(date);
                     String rate = currencyEntry.getRate();
-                    mRateToday = Float.valueOf(rate);
 
                     xAxisValues.add(dateString);
                     dateValueList.add(Float.parseFloat(rate));
                 }
 
-                DrawLineChart drawLineChart = new DrawLineChart(xAxisValues, mLineChart, dateValueList, clickedCurrency + " trend");
+                DrawLineChart drawLineChart = new DrawLineChart(xAxisValues, mLineChart, dateValueList, mClickedCurrency + " trend");
 
             }
         });
 
         mBalance = findViewById(R.id.tv_balance_amount);
         mBalanceUnit = findViewById(R.id.tv_balance_amount_unit);
-        mBalanceUnit.setText("(" + clickedCurrency + ")");
+        mBalanceUnit.setText("(" + mClickedCurrency + ")");
 
         mCost = findViewById(R.id.tv_avg_cost_amount);
         mCostUnit = findViewById(R.id.tv_avg_cost_amount_unit);
@@ -101,7 +106,7 @@ public class DetailActivity extends AppCompatActivity {
         AppExecutors.getInstance().DiskIO().execute(new Runnable() {
             @Override
             public void run() {
-                InvestmentEntry investmentEntry = mDb.investmentDao().loadInvestmentDetail(clickedCurrency);
+                InvestmentEntry investmentEntry = mDb.investmentDao().loadInvestmentDetail(mClickedCurrency);
                 float balance = 0;
                 float cost = 0;
                 if (investmentEntry != null) {
@@ -123,7 +128,7 @@ public class DetailActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String inputString = mAmount.getText().toString();
                 if (Util.isNumber(inputString)) {
-                    makeTransaction(clickedCurrency, Float.valueOf(inputString));
+                    makeTransaction(mClickedCurrency, Float.valueOf(inputString));
                 } else {
                     Toast.makeText(mContext, "Please enter a valid number!", Toast.LENGTH_SHORT).show();
                 }
@@ -135,7 +140,7 @@ public class DetailActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String inputString = mAmount.getText().toString();
                 if (Util.isNumber(inputString)) {
-                    makeTransaction(clickedCurrency, (-1) * Float.valueOf(inputString));
+                    makeTransaction(mClickedCurrency, (-1) * Float.valueOf(inputString));
                 } else {
                     Toast.makeText(mContext, "Please enter a valid number!", Toast.LENGTH_SHORT).show();
                 }
@@ -145,10 +150,28 @@ public class DetailActivity extends AppCompatActivity {
         mSuggestedBuyAmount = findViewById(R.id.tv_suggest_amount);
         mSuggestedBuyAmountUnit = findViewById(R.id.tv_suggest_amount_unit);
         mSuggestedBuyAmountUnit.setText("(" + mainCurrency + ")");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppExecutors.getInstance().DiskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                String rateTodayString = mDb.currencyDao().loadCurrencyRate(Util.getToday(), mClickedCurrency);
+                mRateToday = Float.valueOf(rateTodayString);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setSuggestedInvAmount();
+                    }
+                });
+            }
+        });
     }
 
     private void makeTransaction(final String clickedCurrency, final float inputAmount) {
-
         AppExecutors.getInstance().DiskIO().execute(new Runnable() {
             @Override
             public void run() {
@@ -168,7 +191,6 @@ public class DetailActivity extends AppCompatActivity {
                 updateTransactionHistory(currentBalance, currentCost, clickedCurrency);
             }
         });
-
     }
 
     private void addCurrency2Investment(final String clickedCurrency) {
@@ -215,4 +237,42 @@ public class DetailActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void setSuggestedInvAmount() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        String monInvKey = mContext.getString(R.string.pref_monthly_inv_key);
+        String monInvDefault = mContext.getString(R.string.pref_monthly_inv_default);
+        String monInvValue = sharedPreferences.getString(monInvKey, monInvDefault);
+
+        String minKey = mContext.getString(R.string.pref_history_min_key);
+        String minDefault = mContext.getString(R.string.pref_history_min_default);
+        String minValue = sharedPreferences.getString(minKey, minDefault);
+
+        String maxKey = mContext.getString(R.string.pref_history_max_key);
+        String maxDefault = mContext.getString(R.string.pref_history_max_default);
+        String maxValue = sharedPreferences.getString(maxKey, maxDefault);
+
+        if (Util.isNumber(monInvValue) && Util.isNumber(minValue) && Util.isNumber(maxValue) && Float.valueOf(maxValue) != 0) {
+            double monInv = Double.valueOf(monInvValue);
+            double min = Double.valueOf(minValue);
+            double max = Double.valueOf(maxValue);
+            int interval = getInterval(min, max);
+
+            if (interval <  INVEST_RATIO.length) {
+                double amount = INVEST_RATIO[interval] * monInv;
+                DecimalFormat df = new DecimalFormat("#0.0000");
+                df.format(amount);
+                mSuggestedBuyAmount.setText(amount + "");
+            }
+        } else {
+            Toast.makeText(mContext, "Please udpate preference settings with valid numbers!", Toast.LENGTH_LONG);
+        }
+    }
+
+    private int getInterval(double min, double max) {
+        double intervalToday = (mRateToday / max - 1) * 100;
+        double ratio = ((min / max - 1) * 100) / 5; // Ratio = (min/max - 1) * 100 / # of investment currencies
+        return (int) (intervalToday / ratio);
+    }
+
 }
